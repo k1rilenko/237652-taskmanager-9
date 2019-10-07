f2dffdf505f9c464d5ff35ad5a482c0ac63c6ed4
Roman Kirilenko <cap.spouk@gmail.com>
2019-10-07 21:07:59 +0300

    finish task

---
+++ src/components/data.js
@@ -0,0 +1,42 @@
+const maketask = () => {
+  return {
+    description: [
+      `Изучить теорию`,
+      `Сделать домашку`,
+      `Пройти интенсив на соточку`
+    ][Math.floor(Math.random() * 3)],
+    dueDate: Date.now() + 1 + Math.floor(Math.random() * 7) * 24 * 60 * 60 * 100,
+    tags: new Set([
+      `homework`,
+      `theory`,
+      `practice`,
+      `intensive`,
+      `keks`
+    ]),
+    isRepeating: true,
+    repeatingDays: {
+      'mo': false,
+      'tu': false,
+      'we': Boolean(Math.round(Math.random())),
+      'th': false,
+      'fr': false,
+      'sa': false,
+      'su': false,
+    },
+    color: [
+      `black`,
+      `yellow`,
+      `blue`,
+      `green`,
+      `pink`,
+    ][Math.floor(Math.random() * 5)],
+    isFavorite: Boolean(Math.round(Math.random())),
+    isArchive: Boolean(Math.round(Math.random()))
+  };
+};
+const allTasks = [];
+for (let i = 0; i < 27; i++) {
+  allTasks.push(maketask());
+}
+export {allTasks};
+
--- src/main.js
+++ src/main.js
@@ -6,18 +6,31 @@ import {board} from './components/board.js';
 import {boardFilter} from './components/boardFilter.js';
 import {boardTasks} from './components/boardTasks.js';
 import {button} from './components/button.js';
-
+import {allTasks} from './components/data.js';
+import {filterData} from './components/filterData.js';
+import {cardEdit} from './components/card-edit.js';
 const render = (block, template, position = `afterend`) => {
   const element = document.querySelector(block);
   element.insertAdjacentHTML(position, template);
 };
+const loadTasks = (data) => {
+  const dataForRender = data.splice(1, 8);
+  dataForRender.forEach((element) => {
+    render(`.board__tasks`, card(element), `beforeend`);
+  });
+  if (dataForRender.length < 8) {
+    loadButton.style.display = `none`;
+  }
+};
 render(`.control__title`, menu());
 render(`.main__control`, search());
-render(`.main__search`, filter());
+render(`.main__search`, filter(filterData));
 render(`.main__filter`, board());
 render(`.board`, boardFilter(), `beforeend`);
 render(`.board`, boardTasks(), `beforeend`);
-for (let i = 0; i < 3; i++) {
-  render(`.board__tasks`, card(), `beforeend`);
-}
+render(`.board__tasks`, cardEdit(allTasks[0]), `beforeend`);
 render(`.board__tasks`, button());
+const loadButton = document.querySelector(`.load-more`);
+loadButton.addEventListener(`click`, function () {
+  loadTasks(allTasks);
+});
